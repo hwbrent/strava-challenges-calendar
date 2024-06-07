4c90efe24650ba1563ba0916f5a6433e6f27d1df
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-06-07 19:31:46 +0100

    Only use one URL

--- src/main.py
+++ src/main.py
@@ -2,14 +2,15 @@ from pprint import PrettyPrinter
 
 import requests
 
-# These urls yield HTML pages containing the challenges. There is a `type`
-# parameter which determines whether we get the past, present or future
-# challenges.
+# This url yields an HTML paeg containing the challenges. The `type`
+# parameter determines whether we get past, present or future challenges.
 # I found these urls by looking at the "Network" tab of the FireFox devtools
 BASE_URL = "https://www.kom.club/sections/challenges_amz.asp?filter=all&type="
-PASSED_URL = BASE_URL + "complete"
-ACTIVE_URL = BASE_URL + "active"
-UPCOMING_URL = BASE_URL + "upcoming"
+types = (
+    "complete",  # "Passed"
+    "active",  # "Active"
+    "upcoming",  # "Up Coming"
+)
 
 
 def main():
