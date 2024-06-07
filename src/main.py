5d68698b7d93df9f48a47bd77e2c7ae94c2195ee
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-06-07 19:28:48 +0100

    Add all possible urls

--- src/main.py
+++ src/main.py
@@ -2,6 +2,15 @@ from pprint import PrettyPrinter
 
 import requests
 
+# These urls yield HTML pages containing the challenges. There is a `type`
+# parameter which determines whether we get the past, present or future
+# challenges.
+# I found these urls by looking at the "Network" tab of the FireFox devtools
+BASE_URL = "https://www.kom.club/sections/challenges_amz.asp?filter=all&type="
+PASSED_URL = BASE_URL + "complete"
+ACTIVE_URL = BASE_URL + "active"
+UPCOMING_URL = BASE_URL + "upcoming"
+
 
 def main():
     pass
