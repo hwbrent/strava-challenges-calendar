b89ddbd1a80728860feb3684d9acea46f247bee7
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-06-13 10:36:05 +0100

    Add function to get URLs of challenges

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from pprint import PrettyPrinter
 
 import requests
+from bs4 import BeautifulSoup
 
 # This url yields an HTML paeg containing the challenges. The `type`
 # parameter determines whether we get past, present or future challenges.
@@ -29,8 +30,33 @@ def get_kom_club_html(_type=types[1]) -> str:
     return html
 
 
+def get_strava_challenge_urls(kom_club_html: str) -> set[str]:
+    """
+    Given the HTML for the kom.club page containing all the challenges, this
+    function gets the Strava URL for each of the challenges, and returns them
+    as a set of strings
+    """
+
+    # Specify that we're parsing HTML, rather than XML or w/e
+    soup = BeautifulSoup(kom_club_html, features="html.parser")
+
+    # From manual inspection, there are a bunch of <a>s whose href attribute
+    # is the url that we want. The url always starts with
+    # 'http://www.strava.com/challenges/'. So we can use CSS selectors to
+    # find these tags based off that attribute
+
+    a_tags = soup.select("a[href^='http://www.strava.com/challenges/']")
+
+    # We use a 'set' because otherwise there are two of each URL
+    hrefs = set([tag["href"] for tag in a_tags])
+
+    return hrefs
+
+
 def main():
     html = get_kom_club_html()
+    challenge_urls = get_strava_challenge_urls(html)
+    print(challenge_urls)
 
 
 if __name__ == "__main__":
